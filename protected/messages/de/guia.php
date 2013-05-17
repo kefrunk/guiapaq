8c8d0d1331af7d6e2553564157efbfd07f62ea8b
Eduardo <edwargan@gmail.com>
2013-05-17 07:59:37 +0200

    hoy

---
+++ protected/messages/de/guia.php
@@ -0,0 +1,19 @@
+<?php 
+return array(
+	'elchetitulo' => 'Zone Elche',
+	'vinalopotitulo' => 'Vinalopo Medio und Alto',
+	'parrafo 1' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere fermentum elit sit amet ornare. Nam tincidunt massa vel lectus ultrices et imperdiet mauris porttitor. Nam in lectus mauris. Nulla vel elit at nibh lacinia mollis at ac sem. Mauris congue ligula a ligula vulputate a commodo nunc iaculis. Duis pharetra suscipit aliquam. Ut aliquet vehicula nisl in egestas. Quisque quis adipiscing arcu.',
+
+	'parrafo 2' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere fermentum elit sit amet ornare. Nam tincidunt massa vel lectus ultrices et imperdiet mauris porttitor. Nam in lectus mauris. Nulla vel elit at nibh lacinia mollis at ac sem. Mauris congue ligula a ligula vulputate a commodo nunc iaculis. Duis pharetra suscipit aliquam. Ut aliquet vehicula nisl in egestas. Quisque quis adipiscing arcu.',
+
+	'parrafo 3' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere fermentum elit sit amet ornare. Nam tincidunt massa vel lectus ultrices et imperdiet mauris porttitor. Nam in lectus mauris. Nulla vel elit at nibh lacinia mollis at ac sem. Mauris congue ligula a ligula vulputate a commodo nunc iaculis. Duis pharetra suscipit aliquam. Ut aliquet vehicula nisl in egestas. Quisque quis adipiscing arcu.',
+
+	'parrafo 4' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere fermentum elit sit amet ornare. Nam tincidunt massa vel lectus ultrices et imperdiet mauris porttitor. Nam in lectus mauris. Nulla vel elit at nibh lacinia mollis at ac sem. Mauris congue ligula a ligula vulputate a commodo nunc iaculis. Duis pharetra suscipit aliquam. Ut aliquet vehicula nisl in egestas. Quisque quis adipiscing arcu.',
+
+	'parrafo 5' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere fermentum elit sit amet ornare. Nam tincidunt massa vel lectus ultrices et imperdiet mauris porttitor. Nam in lectus mauris. Nulla vel elit at nibh lacinia mollis at ac sem. Mauris congue ligula a ligula vulputate a commodo nunc iaculis. Duis pharetra suscipit aliquam. Ut aliquet vehicula nisl in egestas. Quisque quis adipiscing arcu.',
+
+	'subtitulo 1' => 'Primer subtítulo',
+	'subtitulo 2' => 'Segundo subtítulo',
+	'subtitulo 3' => 'Tercer subtítulo',
+);
+?>
